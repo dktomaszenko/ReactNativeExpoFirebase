dad1f8b525e8d8cc4d0a8cef9c51d03d23e5382e
dktomaszenko <dktomaszenko@gmail.com>
2018-10-23 15:37:24 +0200

    subida

--- todo-app/src/components/TodoForm.js
+++ todo-app/src/components/TodoForm.js
@@ -22,8 +22,18 @@ export default class TodoForm extends Component {
     }
 
     render(){
+        const {todo} = this.state;
         return (
-            <form onSubmit={e => this.addTodo(e)}></form>
+            <form onSubmit={e => this.addTodo(e)}>
+                <label htmlFor="todo" className="sr-only">Todo</label>
+                <input
+                    type="text"
+                    className="form-control"
+                    id="todo"
+                    value={todo}
+                    onChange={this.updateTodo.bind(this)}
+                />
+            </form>
         )
     }
 }
